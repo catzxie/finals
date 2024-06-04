244cebadbe93eef0dbb9a3e6871c9afced5c31ee
catzxie <catherinebelen192@gmail.com>
2024-06-04 22:38:10 +0800

    Delete users directory

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def aboutme(request):
-    return render(request, 'html/belen.html')
-
-def contact(request):
-    return render(request, 'html/contact.html')
